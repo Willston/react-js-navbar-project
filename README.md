0f2a0178cd96712c816491adb079a15a0adfa864
Willston <stonw94@gmail.com>
2021-01-17 14:35:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-j5mgkd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-j5mgkd)
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,36 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-link'
+  },
+
+{
+    title: 'Services',
+    url: '#',
+    cName: 'nav-link'
+  },
+
+{
+    title: 'Products',
+    url: '#',
+    cName: 'nav-link'
+  },
+
+{
+    title: 'Contact Us',
+    url: '#',
+    cName: 'nav-link'
+  },
+
+  {
+    title: 'Sign Up',
+    url: '#',
+    cName: 'nav-link-mobile'
+  },
+
+
+
+
+  
+]
